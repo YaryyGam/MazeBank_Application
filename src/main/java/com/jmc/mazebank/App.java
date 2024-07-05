e54c8df42e2bdb00490645e4b0aefef1ebe56d9e
Yaroslav <gamerplay0606@gmail.com>
2024-07-05 12:32:02 +0300

    Updates#1

--- src/main/java/com/jmc/mazebank/App.java
+++ src/main/java/com/jmc/mazebank/App.java
@@ -1,4 +1,21 @@
 package com.jmc.mazebank;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/Login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/main/java/com/jmc/mazebank/Controllers/LoginController.java
+++ src/main/java/com/jmc/mazebank/Controllers/LoginController.java
@@ -1,4 +1,15 @@
 package com.jmc.mazebank.Controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    public ChoiceBox acc_selector;
+    public Label payee_adress_lbl;
+    public TextField payee_adress_fld;
+    public TextField password_fld;
+    public Button loggin_btn;
+    public Label error_lbl;
 }
