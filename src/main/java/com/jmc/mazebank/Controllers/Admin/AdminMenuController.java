4e66f13f2a91218d56b76a6c63a3d86a89ce222d
YaryyGam <gamerplay0606@gmail.com>
2024-07-12 02:44:13 +0300

    #Update6

--- src/main/java/com/jmc/mazebank/Controllers/Admin/AdminMenuController.java
+++ src/main/java/com/jmc/mazebank/Controllers/Admin/AdminMenuController.java
@@ -1,5 +1,6 @@
 package com.jmc.mazebank.Controllers.Admin;
 
+import com.jmc.mazebank.Models.Model;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
@@ -14,6 +15,11 @@ public class AdminMenuController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        addListeners();
+    }
 
+    private void addListeners(){}
+    private void onCreateClient(){
+        Model.getInstance().getViewFactory().getAdminSelectedMenuItem().set("CreateClient");
     }
 }
