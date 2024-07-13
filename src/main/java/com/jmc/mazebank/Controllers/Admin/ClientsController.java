cd6f56d0823ef353e2529cd5bd5bb22afe4232fa
Yaroslav <gamerplay0606@gmail.com>
2024-07-13 15:30:14 +0300

    #Update6.3

--- src/main/java/com/jmc/mazebank/Controllers/Admin/ClientsController.java
+++ src/main/java/com/jmc/mazebank/Controllers/Admin/ClientsController.java
@@ -1,4 +1,16 @@
 package com.jmc.mazebank.Controllers.Admin;
 
-public class ClientsController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientsController implements Initializable {
+    public ListView clients_listview;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
