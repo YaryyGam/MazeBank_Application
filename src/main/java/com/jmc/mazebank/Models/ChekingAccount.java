f7ce8922a73c307de3e6acaa79e76f7624aa3ff7
Yaroslav <gamerplay0606@gmail.com>
2024-07-16 14:37:17 +0300

    #Update7

---
+++ src/main/java/com/jmc/mazebank/Models/ChekingAccount.java
@@ -0,0 +1,16 @@
+package com.jmc.mazebank.Models;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class ChekingAccount extends Account{
+    // The number of transactions that client  can do per day
+    private final IntegerProperty transactionLimit;
+
+    public ChekingAccount(String owner, String accountNumber, double balance, int transactionLimit){
+        super(owner, accountNumber, balance);
+        this.transactionLimit = new SimpleIntegerProperty(this, "Transaction Limit", transactionLimit);
+    }
+
+    public IntegerProperty transactionLimitProperty(){return transactionLimit;}
+}
---
+++ src/main/java/com/jmc/mazebank/Models/SavingsAccount.java
@@ -0,0 +1,18 @@
+package com.jmc.mazebank.Models;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+
+public class SavingsAccount extends Account{
+    // The withdrawal limit from the saving account
+
+    private final DoubleProperty withdrawalLimit;
+
+    public SavingsAccount(String owner, String accountNumber, double balance, double withdrawalLimit){
+        super(owner, accountNumber, balance);
+        this.withdrawalLimit = new SimpleDoubleProperty(this, "Withdrawal Limit", withdrawalLimit);
+    }
+
+    public DoubleProperty withdrawalLimitProperty(){return withdrawalLimit;}
+}
